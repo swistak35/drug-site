069b1325dcdb09875b31512e8d927367138a8353
Rafał Łasocha <me+dev@swistak35.com>
2013-02-11 17:06:06 +0100

    day at drug remainder views

--- app/mailers/remainder_mailer.rb
+++ app/mailers/remainder_mailer.rb
@@ -3,9 +3,21 @@ class RemainderMailer < ActionMailer::Base
 
   def day_before(receiver, event)
     @event = event
+    generate_cat_stuff
+
+    mail to: receiver.email, subject: "#{@event.title} IS COMING"
+  end
+
+  def today(receiver, event)
+    @event = event
+    generate_cat_stuff
+
+    mail to: receiver.email, subject: "#{@event.title} IS COMING"
+  end
+
+private
+  def generate_cat_stuff
     @width = 100 + rand(701)
     @height = (@width.to_f * 3 / 4).to_i
-    
-    mail to: receiver.email, subject: "#{@event.title} IS COMING"
   end
 end
